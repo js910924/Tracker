d513f7ff6855ddaa96aad0ed2f9845dab352df37
js910924 <js910924@gmail.com>
2020-08-16 22:16:26 +0800

    implement ConsumptionController

---
+++ ProjectTemp.API/Controllers/ConsumptionController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectTemp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ConsumptionController : ControllerBase
+    {
+        private static List<string> Summaries = new List<string>{};
+
+        private readonly ILogger<ConsumptionController> _logger;
+
+        public ConsumptionController(ILogger<ConsumptionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            _logger.LogInformation("Call Consumption GetAll method");
+            return new JsonResult(Summaries);
+        }
+
+        [HttpGet]
+        public ActionResult Get(string refNo)
+        {
+            ValidateRefNo(refNo);
+            _logger.LogInformation($"Call Consumption Get method: RefNo = {refNo}");
+            return new JsonResult(Summaries.FirstOrDefault(x => x == refNo));
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string refNo)
+        {
+            ValidateRefNo(refNo);
+            _logger.LogInformation($"Delete Consumption RefNo = {refNo}");
+            return new JsonResult(Summaries.Remove(refNo));
+        }
+
+        [HttpGet]
+        public ActionResult Insert(string refNo)
+        {
+            ValidateRefNo(refNo);
+            _logger.LogInformation($"Insert Consumption RefNo = {refNo}");
+            Summaries.Add(refNo);
+            return new JsonResult(Summaries);
+        }
+
+        private void ValidateRefNo(string refNo)
+        {
+            if (string.IsNullOrEmpty(refNo))
+            {
+                throw new Exception($"Invalid RefNo = {refNo}");
+            }
+        }
+    }
+}
